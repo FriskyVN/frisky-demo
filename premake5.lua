14161bcef5bff35c54500794cbe7e832127b5db5
Tufted Tail <tufted.tail@gmail.com>
2019-06-21 10:05:42 -0700

    Updated premake: SDL_ttf

--- premake5.lua
+++ premake5.lua
@@ -77,6 +77,7 @@ workspace "FriskyDemo"
       "SDL2",
       "SDL2main",
       "SDL2_image",
+      "SDL2_ttf",
       "lua53"
     }
 
@@ -104,7 +105,8 @@ workspace "FriskyDemo"
         "external/frisky-core/include",
         "external/lua-vc/include",
         "external/sdl2-vc/include",
-        "external/sdl2image-vc/include"
+        "external/sdl2image-vc/include",
+        "external/sdl2ttf-vc/include"
       }
 
     filter {}
@@ -113,32 +115,37 @@ workspace "FriskyDemo"
     local lua       = cwd .. "\\external\\lua-vc\\lib\\"
     local sdl2      = cwd .. "\\external\\sdl2-vc\\lib\\"
     local sdl2image = cwd .. "\\external\\sdl2image-vc\\lib\\"
+    local sdl2ttf   = cwd .. "\\external\\sdl2ttf-vc\\lib\\"
     local dest      = cwd .. "\\bin\\%{cfg.buildcfg}\\%{cfg.platform}"
 
     filter { "system:windows", "action:vs*", "platforms:*32" }
       libdirs {
         "external/lua-vc/lib/x86",
         "external/sdl2-vc/lib/x86/",
-        "external/sdl2image-vc/lib/x86"
+        "external/sdl2image-vc/lib/x86",
+        "external/sdl2ttf-vc/lib/x86"
       }
 
       postbuildcommands {
         "copy \"" .. lua       .. "x86\\lua53.dll\" \""      .. dest .. "\"",
         "copy \"" .. sdl2      .. "x86\\SDL2.dll\" \""       .. dest .. "\"",
-        "copy \"" .. sdl2image .. "x86\\SDL2_image.dll\" \"" .. dest .. "\""
+        "copy \"" .. sdl2image .. "x86\\SDL2_image.dll\" \"" .. dest .. "\"",
+        "copy \"" .. sdl2ttf   .. "x86\\SDL2_ttf.dll\" \""   .. dest .. "\""
       }
 
     filter { "system:windows", "action:vs*", "platforms:*64" }
       libdirs {
         "external/lua-vc/lib/x64",
         "external/sdl2-vc/lib/x64/",
-        "external/sdl2image-vc/lib/x64"
+        "external/sdl2image-vc/lib/x64",
+        "external/sdl2ttf-vc/lib/x64"
       }
 
       postbuildcommands {
         "copy \"" .. lua       .. "x64\\lua53.dll\" \""      .. dest .. "\"",
         "copy \"" .. sdl2      .. "x64\\SDL2.dll\" \""       .. dest .. "\"",
-        "copy \"" .. sdl2image .. "x64\\SDL2_image.dll\" \"" .. dest .. "\""
+        "copy \"" .. sdl2image .. "x64\\SDL2_image.dll\" \"" .. dest .. "\"",
+        "copy \"" .. sdl2ttf   .. "x64\\SDL2_ttf.dll\" \""   .. dest .. "\""
       }
 
     filter {}
